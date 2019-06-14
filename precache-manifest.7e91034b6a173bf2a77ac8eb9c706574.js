afcf7914893e7aab660fff0c3504ad5b4c176965
Rakesh Guha <guha.rakesh@gmail.com>
2019-06-14 12:28:28 -0400

    Updates

---
+++ precache-manifest.7e91034b6a173bf2a77ac8eb9c706574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effcc43ab8f627a61726dab8fcc81ac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e19cbdd44fa9ba11d0f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1e19cbdd44fa9ba11d0f",
+    "url": "/static/js/main.fe968fdb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
