7ce032b0cfdefd3f42fb75fd1bd27207d74f01b3
Rakesh Guha <guha.rakesh@gmail.com>
2019-06-14 12:22:06 -0400

    Pages

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Fan page coming soon, by A, B, and R
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCou_foVGi73d9ieCelFUSUQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Megan Fox Unlocked
         </a>
       </header>
     </div>
